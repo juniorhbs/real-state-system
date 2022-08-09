7cf7d27e211a40cd08855c11729a3820b878a8b7
Junior <junioirhbs@gmail.com>
2022-08-09 12:20:26 -0300

    Filter Page: Structure and responsibility

--- widget/filtro.php
+++ widget/filtro.php
@@ -1,1 +1,162 @@
-FILTRO
+<div class="main_filter bg-light py-5">
+    <div class="container">
+        <section class="row">
+            <div class="col-12">
+                <h2 class="text-front icon-filter mb-5">Filtro</h2>
+            </div>
+
+            <div class="col-12 col-md-4 bg-white">
+                <form action="" class="w-100 mb-5">
+                    <div class="row">
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Comprar ou Alugar</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha...">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Comprar ou Alugar</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha...">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Comprar ou Alugar</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha..." multiple
+                                    data-actions-box="true">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-12">
+                            <label for="search" class="mb-2 text-front">Comprar ou Alugar</label>
+                            <select name="search" id="search" class="selectpicker" title="Escolha...">
+                                <option value="">Comprar</option>
+                                <option value="">Alugar</option>
+                            </select>
+                        </div>
+
+                        <div class="col-12 mt-3 text-right">
+                            <button class="btn btn-front icon-search">Pesquisar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="col-12 col-md-8">
+                <section class="main_properties">
+                    <div class="container">
+                        <div class="row">
+                            <article class="col-12 col-md-12 col-lg-6 mb-4">
+                                <div class="card main_properties_item">
+                                    <div class="img-responsive-16by9">
+                                        <a href="">
+                                            <img src="properties/1/5a3571ab-4d76-466f-8246-eff8cb98cedd.jpg" class="card-img-top" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h2><a href="" class="text-front">Linda Casa no Campeche com vista para o Mar</a></h2>
+                                        <p class="main_properties_item_category">Imóvel Residêncial</p>
+                                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i>
+                                        </p>
+                                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                                        <a href="" class="btn btn-front btn-block"> Ver Imóvel</a>
+                                    </div>
+                                    <div class="card-footer d-flex text-muted">
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="">
+                                            <p class="text-muted">1</p>
+                                        </div>
+
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/garage.png" class="img-fluid" alt="">
+                                            <p class="text-muted">4</p>
+                                        </div>
+
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/util-area.png" class="img-fluid" alt="">
+                                            <p class="text-muted">180m²</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+
+                            <article class="col-12 col-md-12 col-lg-6 mb-4">
+                                <div class="card main_properties_item">
+                                    <div class="img-responsive-16by9">
+                                        <a href="">
+                                            <img src="properties/2/a56fc386-3790-4e98-a72c-3b09d24adecd.jpg" class="card-img-top" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h2><a href="" class="text-front">Linda Casa no Campeche com vista para o Mar</a></h2>
+                                        <p class="main_properties_item_category">Imóvel Residêncial</p>
+                                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i>
+                                        </p>
+                                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                                        <a href="" class="btn btn-front btn-block"> Ver Imóvel</a>
+                                    </div>
+                                    <div class="card-footer d-flex text-muted">
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="">
+                                            <p class="text-muted">1</p>
+                                        </div>
+
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/garage.png" class="img-fluid" alt="">
+                                            <p class="text-muted">4</p>
+                                        </div>
+
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/util-area.png" class="img-fluid" alt="">
+                                            <p class="text-muted">180m²</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+
+                            <article class="col-12 col-md-12 col-lg-6 mb-4">
+                                <div class="card main_properties_item">
+                                    <div class="img-responsive-16by9">
+                                        <a href="">
+                                            <img src="properties/3/1c7275a0-c84e-4f25-9247-9b56664eb294.jpg" class="card-img-top" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h2><a href="" class="text-front">Linda Casa no Campeche com vista para o Mar</a></h2>
+                                        <p class="main_properties_item_category">Imóvel Residêncial</p>
+                                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i>
+                                        </p>
+                                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                                        <a href="" class="btn btn-front btn-block"> Ver Imóvel</a>
+                                    </div>
+                                    <div class="card-footer d-flex text-muted">
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/bed.png" class="img-fluid" alt="">
+                                            <p class="text-muted">1</p>
+                                        </div>
+
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/garage.png" class="img-fluid" alt="">
+                                            <p class="text-muted">4</p>
+                                        </div>
+
+                                        <div class="col-4 main_properties_item_features text-center">
+                                            <img src="assets/images/icons/util-area.png" class="img-fluid" alt="">
+                                            <p class="text-muted">180m²</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+    </div>
+</div>
