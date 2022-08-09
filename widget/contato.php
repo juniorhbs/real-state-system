f72e4a5995154601cfb0f68f8de2eeda3304a24a
Junior <junioirhbs@gmail.com>
2022-08-09 14:31:57 -0300

    Page contact: Structure and responsibility

--- widget/contato.php
+++ widget/contato.php
@@ -1,1 +1,63 @@
-CONTATO
+<div class="main_contact py-5 bg-light text-center">
+    <div class="container">
+        <h1 class="text-front">Entre em Contato Conosco</h1>
+        <p class="mb-0">Quer conversar com um corretor exclusivo e ter o atendimento diferenciado em busca do seu imóvel
+            dos sonhos?</p>
+        <p>Preencha o formulário abaixo e vamos lhe direcionar para alguém que entende a sua necessidade!</p>
+
+        <div class="row text-left">
+            <form action="">
+                <h2 class="icon-envelope text-black-50"> Envie um e-mail</h2>
+
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Insira seu nome">
+                </div>
+
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="Insira seu melhor e-mail">
+                </div>
+
+                <div class="form-group">
+                    <input type="tel" class="form-control" placeholder="Insira seu telefone com DDD">
+                </div>
+
+                <div class="form-group">
+                    <textarea name="teste" id="" cols="30" rows="5" placeholder="Escreva sua mensagem..."
+                              class="form-control"></textarea>
+                </div>
+
+                <div class="form-group text-right">
+                    <button class="btn btn-front">Enviar Contato</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="main_contact_types py-5">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12 col-md-4">
+                <h2 class="icon-envelope">Por E-mail</h2>
+                <p>Nossos atendentes vão entrar em contato com você assim que possível.</p>
+                <p class="pt-2"><a href="#" class="text-front">curso@upinside.com.br</a></p>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <h2 class="icon-phone">Por telefone</h2>
+                <p>Estamos disposnível no número abaixo no horário comercial.</p>
+                <p class="text-front pt-2">+55 (11) 3322-1245</p>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <h2 class="icon-share-alt">Redes sociais</h2>
+                <p>Fique por dentro de tudo o que a gente compartilha em nossas redes sociais.</p>
+                <p>
+                    <button class="btn btn-front icon-facebook icon-notext"></button>
+                    <button class="btn btn-front icon-twitter icon-notext"></button>
+                    <button class="btn btn-front icon-instagram icon-notext"></button>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
